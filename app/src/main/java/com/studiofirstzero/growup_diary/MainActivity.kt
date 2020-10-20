c494c629e8a45a173df11cbbf42af7a33fa08cbe
RobertHan96 <yshan4329@gmail.com>
2020-10-21 00:33:26 +0900

    Initial commit, 블루투스 통신을 위해 필요한 권한 설정

---
+++ app/src/main/java/com/studiofirstzero/growup_diary/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.studiofirstzero.growup_diary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
