989d5b7456203cfc2591627eafa56a4b36166784
RobertHan96 <yshan4329@gmail.com>
2020-10-21 00:53:42 +0900

    2nd commit : Android-BluetoothSPPLibrary 라이브러리 추가 : 블루투스 켜짐/꺼짐 감지로직 추가

--- app/src/main/java/com/studiofirstzero/growup_diary/MainActivity.kt
+++ app/src/main/java/com/studiofirstzero/growup_diary/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.studiofirstzero.growup_diary
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import app.akexorcist.bluetotohspp.library.BluetoothSPP
+import app.akexorcist.bluetotohspp.library.BluetoothState
+import app.akexorcist.bluetotohspp.library.DeviceList
+
 
 class MainActivity : BaseActivity() {
+    var bt = BluetoothSPP(mContext)
+
     override fun setupEvents() {
 
     }
@@ -14,6 +21,20 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        onStart()
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (!bt.isBluetoothEnabled ) {
+            Log.d("log", "블루투스 꺼짐")
+        } else {
+            Log.d("log", "블루투스 켜짐, 장치 검색 시작...")
+            bt.startService(BluetoothState.DEVICE_OTHER)
+            val intent = Intent(mContext, DeviceList::class.java)
+            startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE)
+        }
     }
 
 }
