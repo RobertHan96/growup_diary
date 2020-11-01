8faa17a024e1341ca368e192de536a36fd5307a5
RobertHan96 <yshan4329@gmail.com>
2020-11-01 20:56:50 +0900

    9th commit : 작성화면에서 블루투스로 값 측정, 선택하는 로직 추가

--- app/src/main/java/com/studiofirstzero/growup_diary/MainActivity.kt
+++ app/src/main/java/com/studiofirstzero/growup_diary/MainActivity.kt
@@ -15,56 +15,19 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : BaseActivity() {
-    lateinit var bt : BluetoothSPP
     var db = FirebaseFirestore.getInstance()
-    val BLUETOOTH_REQ = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        bt = BluetoothSPP(mContext)
         setValues()
         setupEvents()
-
-        if (!bt.isBluetoothAvailable()) { //블루투스 사용 불가
-            Toast.makeText(mContext, "Bluetooth is not available", LENGTH_SHORT).show();
-            finish();
-        }
-
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        bt.stopService()
-    }
     override fun setupEvents() {
         find_bt_device_btn.setOnClickListener {
-            if (bt.serviceState == BluetoothState.STATE_CONNECTED) {
-                bt.disconnect()
-            } else {
-                val intent = Intent(mContext, DeviceList::class.java)
-                startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE)
-            }
+
         }
-
-        bt.setOnDataReceivedListener { data, message ->
-            Toast.makeText(mContext, "vlaue ${message}", LENGTH_SHORT).show();
-        }
-
-
-        bt.setBluetoothConnectionListener(object : BluetoothConnectionListener {
-            override fun onDeviceConnected(name: String, address: String) {
-                Toast.makeText(mContext, "측정기와 연결되었습니다.", LENGTH_SHORT).show();
-            }
-
-            override fun onDeviceDisconnected() {
-                Toast.makeText(mContext, "측정기와 연결이 해제되었습니다.", LENGTH_SHORT).show();
-            }
-
-            override fun onDeviceConnectionFailed() {
-                Toast.makeText(mContext, "연결된 측정기가 없습니다, 연결 버튼을 통해 연결해주세요.", LENGTH_SHORT).show();
-            }
-        })
 
         write_diary_btn.setOnClickListener {
             val user: MutableMap<String, Any> = HashMap()
@@ -109,40 +72,7 @@ class MainActivity : BaseActivity() {
         }
     }
 
-
     override fun setValues() {
 
     }
-
-    override fun onStart() {
-        super.onStart()
-        if (!bt.isBluetoothEnabled ) {
-            Log.d("log", "블루투스 켜짐, 데이터 수신 시작...")
-            val intent = Intent(mContext, DeviceList::class.java)
-            startActivityForResult(intent, BluetoothState.REQUEST_ENABLE_BT)
-        } else {
-            Log.d("log", "블루투스 꺼짐")
-            if (!bt.isServiceAvailable) {
-                bt.setupService()
-                bt.startService(BluetoothState.DEVICE_OTHER)
-            }
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == BluetoothState.REQUEST_CONNECT_DEVICE) {
-            if (resultCode == Activity.RESULT_OK) {
-                bt.connect(data)
-            }
-        } else if (requestCode == BluetoothState.REQUEST_ENABLE_BT) {
-            if (resultCode == Activity.RESULT_OK) {
-                bt.setupService()
-                bt.startService(BluetoothState.DEVICE_OTHER)
-            } else {
-                Toast.makeText(mContext, "Bluetooth is not available", LENGTH_SHORT).show();
-            }
-        }
-    }
-
 }
--- app/src/main/java/com/studiofirstzero/growup_diary/WriteDiaryActivity.kt
+++ app/src/main/java/com/studiofirstzero/growup_diary/WriteDiaryActivity.kt
@@ -1,8 +1,14 @@
 package com.studiofirstzero.growup_diary
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import app.akexorcist.bluetotohspp.library.BluetoothSPP
+import app.akexorcist.bluetotohspp.library.BluetoothState
+import app.akexorcist.bluetotohspp.library.DeviceList
 import kotlinx.android.synthetic.main.activity_write_diary.*
 
 // 사진과 함께 글을 작성하는 곳, 게시판 형태로 작성
@@ -10,6 +16,20 @@ import kotlinx.android.synthetic.main.activity_write_diary.*
 // 작성 내용 : 제목, 내용, 사진 1장, 키 (다이얼로그 창으로 구현 - 파이버에이스와 연동)
 
 class WriteDiaryActivity : BaseActivity() {
+    lateinit var bt : BluetoothSPP
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write_diary)
+        setValues()
+        setupEvents()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        bt.stopService()
+    }
+
     override fun setupEvents() {
         writePostBtn.setOnClickListener {
             val postDetailActivity = Intent( mContext, PostDetailActivity::class.java)
@@ -17,16 +37,74 @@ class WriteDiaryActivity : BaseActivity() {
 
         }
 
+        getMeasureValuesBtn.setOnClickListener {
+            if (bt.serviceState == BluetoothState.STATE_CONNECTED) {
+                bt.disconnect()
+            } else {
+                val intent = Intent(mContext, DeviceList::class.java)
+                startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE)
+            }
+        }
+
+        bt.setOnDataReceivedListener { data, message ->
+            measuredValueText.text = message
+        }
+
+        bt.setBluetoothConnectionListener(object : BluetoothSPP.BluetoothConnectionListener {
+            override fun onDeviceConnected(name: String, address: String) {
+                Toast.makeText(mContext, "측정기와 연결되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+
+            override fun onDeviceDisconnected() {
+                Toast.makeText(mContext, "측정기와 연결이 해제되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+
+            override fun onDeviceConnectionFailed() {
+                Toast.makeText(mContext, "연결된 측정기가 없습니다, 연결 버튼을 통해 연결해주세요.", Toast.LENGTH_SHORT).show();
+            }
+        })
+
+
     }
 
     override fun setValues() {
+        bt = BluetoothSPP(mContext)
+        if (!bt.isBluetoothAvailable()) { //블루투스 사용 불가
+            Toast.makeText(mContext, "Bluetooth is not available", Toast.LENGTH_SHORT).show();
+            finish();
+        }
 
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_write_diary)
-        setupEvents()
-        setValues()
+    override fun onStart() {
+        super.onStart()
+        if (!bt.isBluetoothEnabled ) {
+            Log.d("log", "블루투스 켜짐, 데이터 수신 시작...")
+            val intent = Intent(mContext, DeviceList::class.java)
+            startActivityForResult(intent, BluetoothState.REQUEST_ENABLE_BT)
+        } else {
+            Log.d("log", "블루투스 꺼짐")
+            if (!bt.isServiceAvailable) {
+                bt.setupService()
+                bt.startService(BluetoothState.DEVICE_OTHER)
+            }
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == BluetoothState.REQUEST_CONNECT_DEVICE) {
+            if (resultCode == Activity.RESULT_OK) {
+                bt.connect(data)
+            }
+        } else if (requestCode == BluetoothState.REQUEST_ENABLE_BT) {
+            if (resultCode == Activity.RESULT_OK) {
+                bt.setupService()
+                bt.startService(BluetoothState.DEVICE_OTHER)
+            } else {
+                Toast.makeText(mContext, "Bluetooth is not available", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
